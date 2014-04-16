d278ab7cc4e9a33500dc5675385a2e6b6bbe894d
Eugene Starov <chudesno@gmail.com>
2014-04-16 20:03:10 +0300

    Interpolating the result

--- src/filter.js
+++ src/filter.js
@@ -1,5 +1,5 @@
 angular.module('gettext').filter('translate', function (gettextCatalog, $interpolate, $parse) {
-    return function (input) {
-        return gettextCatalog.getString(input);
+    return function (input, params) {
+        return $interpolate(gettextCatalog.getString(input))(params);
     };
 });
