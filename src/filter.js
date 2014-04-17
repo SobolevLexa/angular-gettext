4a2fa28bc17f97d93a305bbccafe74bfa568c9c2
Alexey Sobolev <Alexey.Sobolev@teaminternational.com>
2014-04-17 11:48:04 +0300

    fix

--- src/filter.js
+++ src/filter.js
@@ -1,5 +1,9 @@
 angular.module('gettext').filter('translate', function (gettextCatalog, $interpolate, $parse) {
     return function (input, params) {
-        return $interpolate(gettextCatalog.getString(input))(params);
+        if(angular.isObject(params)){
+            return $interpolate(gettextCatalog.getString(input))(params);
+        }
+        return gettextCatalog.getString(input);
+
     };
 });
